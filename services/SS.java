3aebe64ffb8747de2a5382ad8b9693aeead237af
EnriqueSubias <48204227+EnriqueSubias@users.noreply.github.com>
2021-12-13 23:14:26 +0100

    initial commit

---
+++ services/SS.java
@@ -0,0 +1,13 @@
+package services;
+
+/**
+ * External services involved in procedures from population
+ */
+
+public interface SS { // External service for Social Security Govern administration
+
+    // LaboralLifeDoc getLaboralLife(Nif nif) throws NotAffiliatedException, ConnectException; // 4, 5
+
+    //MemberAccreditationDoc getMembAccred(Nif nif) throws NotAffiliatedException, ConnectException; // 6
+    
+}
